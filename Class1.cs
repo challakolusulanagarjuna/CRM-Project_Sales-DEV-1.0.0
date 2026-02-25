8ba8e2a7d37509437523f5dc2ee7dde170778d20
vamsi <vamsi@MYDESKTOP_HOME>
2026-02-25 17:03:44 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Project_Sales_DEV_1._0._0
+{
+    public class Class1
+    {
+    }
+}
